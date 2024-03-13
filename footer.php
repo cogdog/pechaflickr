89559d24c08ace095caff29e3d0d617c24f8b0b1
Alan Levine <cogdogblog@gmail.com>
2024-03-13 14:34:30 -0600

    Update footer.php
    Add license link to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
-<p class="credit"><a href="https://pechaflickr.net/">pechaflickr.net</a> &bull; <a href="notes.php">notes and Q &amp; A</a> &bull; <a href="https://github.com/cogdog/pechaflickr">gitub code</a><br />now available in <a href="http://pechaflickr.de/">german</a><br />pechaflickr is a cogdog production: 
-<a href="https://cog.dog/">cog.dog</a> •  <a href="http://twitter.com/cogdog">@cogdog</a>
+<p class="credit"><a href="https://pechaflickr.net/">pechaflickr.net</a> &bull; <a href="notes.php">notes and Q &amp; A</a> &bull; 
+  <a href="https://github.com/cogdog/pechaflickr">gitub code</a> licensed <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPL3.0</a>a> <br />
+  now available in <a href="http://pechaflickr.de/">german</a><br />
+  pechaflickr is a cogdog production: 
+  <a href="https://cog.dog/">cog.dog</a> •  <a href="http://twitter.com/cogdog">@cogdog</a>
+</p>p>
 </div> <!-- end wrapper -->
 
 <?php if (GOOGLEKEY):?>
